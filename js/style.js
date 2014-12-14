ee6ef5f58b3d00e17e7286dcb620529594820e5f
thanhntp <phuongthanhnguyen692@gmail.com>
2014-12-14 16:31:12 +0700

    update cart

--- js/style.js
+++ js/style.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
+    $(".submit", $("#order-detail")).on("click", function(){
+        $("#order-detail").css({
+            display:"none"
+        });
+        $("#info-cart").css({
+            display:"block"
+        });
+        $(".submit", $("#info-cart")).on("click", function(){
+            $("#info-cart").css({
+                display:"none"
+            });
+            $("#notify").css({
+                display:"block"
+            });
+        })
+    })
 
 })
